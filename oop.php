8b27da392d826a30c0ebbc855f5b4c9567063f3c
Md.Mahadi Hasan <115818114+mahdiswork@users.noreply.github.com>
2024-08-23 17:45:40 +0600

    Update oop.php

--- oop.php
+++ oop.php
@@ -1,95 +1,55 @@
 <?php
 
 class Book {
-
- // TODO: Add properties as Private
-
  private $title,$availableCopies;
 
- 
-
  public function __construct($title, int $availableCopies) {
-
-     // TODO: Initialize properties
-     $this->title = $title;
-     $this->availableCopies = $availableCopies;
-
-     }
-
-// TODO: Add getTitle method
+    $this->title = $title;
+    $this->availableCopies = $availableCopies;
+ }
 
  public function getTitle() {
      return $this->title;
  }
 
- // TODO: Add getAvailableCopies method
-
  public function getAvailableCopies() {
      return $this->availableCopies;
  }
 
-
- // TODO: Add borrowBook method
-
  public function borrowBook() {
-
      if ($this->availableCopies < 0) {
-
          $this->availableCopies = 0;
-
      } else {
-
          $this->availableCopies = $this->availableCopies - 1;
-
      }
-
 }
 
-
- // TODO: Add returnBook method
 public function returnBook(){
     $this->availableCopies = $this->availableCopies + 1;
 }
- 
 
 }
 
-
 class Member {
-
- // TODO: Add properties as Private
 private $name;
 
  public function __construct($name) {
+    $this->name = $name;
+ }
 
-      $this->name = $name;
-
-     }
-
-
- // TODO: Add getName method
  public function getName(){
     return $this->name;
  }
 
- 
-
- // TODO: Add borrowBook method
-
-  public function borrowBook(Book $book){
+ public function borrowBook(Book $book){
     $book->borrowBook();
-  }
-
-
- // TODO: Add returnBook method
+ }
 
  public function returnBook(Book $book){
     $book->returnBook();
  }
 
 }
-
-
 
 $book1 = new Book("The Great Gatsby", 5);
 $book2 = new Book("To Kill a Mockingbird", 3);
